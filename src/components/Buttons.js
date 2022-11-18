62fc8f588a9247405b3c9aa0a100f716c2b46700
Umut Akbayin <umutakbayin@posteo.de>
2022-11-18 22:10:14 +0100

    Add styles

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -4,25 +4,15 @@ import styles from '../styles/Buttons.module.css';
 const Buttons = (props) => {
   return <div className={styles['keyboard']}>
     {props.buttons.map((el, ind) => {
-      return el === "0" ? (
-        <button
-          key={el}
-          className={styles["span-2"]}
-          onClick={() => props.handleClick(el)}
-          value={el}
+      return <button
+        key={el}
+        onClick={() => props.handleClick(el)}
+        value={el}
         >
-          {el}
+        {el}
         </button>
-      ) : (
-        <button
-          key={el}
-          onClick={() => props.handleClick(el)}
-          value={el}
-        >
-          {el}
-        </button>
-      );
-    })}
+    })
+    }
   </div>
 }
 
